ee9318114b67a4ac734f443e402e5184f284c54d
SamuelDAdams <samuel.adams.d@gmail.com>
2021-02-22 20:44:35 -0800

    changed to maxpool2d

--- nets.py
+++ nets.py
@@ -27,7 +27,6 @@ class Net2(nn.Module): #87% with k = [2, 4, 8, 16, 32, 64], k_amount = 64
         
         self.conv1 = nn.ModuleList([nn.Conv1d(in_channels=1, out_channels=channels, kernel_size=k) for k in kernels])
         self.dropout = nn.Dropout(dropout)
-        self.sigmoid = nn.Sigmoid()
         #self.norm = nn.BatchNorm1d()
 
         # self.conv1 = nn.Sequential(
@@ -45,10 +44,11 @@ class Net2(nn.Module): #87% with k = [2, 4, 8, 16, 32, 64], k_amount = 64
     def forward(self, batch):
         x = torch.reshape(batch, (len(batch), 1, 768))
         x = [func.relu(conv(x)) for conv in self.conv1]
-        x = [func.max_pool1d(l, l.size(2)) for l in x]
-        x = torch.cat(x, dim=1).squeeze()
+        # x = [func.max_pool2d(l, (1, l.size(-1))) for l in x]
+        x = [func.max_pool2d(l, (1, l.size(2).item())) for l in x]
+        x = torch.cat(x, dim=1).squeeze(2)
         x = self.dropout(x)
         #out = self.drop(out)
         #logits = self.fc(out.squeeze(2))
         logits = self.lin(x)
-        return self.sigmoid(logits)
+        return logits
